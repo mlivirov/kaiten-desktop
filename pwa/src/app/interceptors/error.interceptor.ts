f08d99abab11f59ebb759d2c9889a0fc3a7e58bd
mlivirov <max.livirov@gmail.com>
2024-08-29 01:58:35 +0300

    feat #13: add error interceptor to show server errors in toast

---
+++ pwa/src/app/interceptors/error.interceptor.ts
@@ -0,0 +1,16 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { inject } from '@angular/core';
+import { ToastService } from '../services/toast.service';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const toastService = inject(ToastService);
+
+  return next(req).pipe(
+    catchError((error: any) => {
+      console.log('error interceptor', error);
+      toastService.error(`Server error: ${error.statusText}`);
+      return throwError(() => error);
+    })
+  );
+};
