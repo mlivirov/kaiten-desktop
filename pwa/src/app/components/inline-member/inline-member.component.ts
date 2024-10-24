5fa0765036e21f196e1837bb22687ff577e704eb
mlivirov <max.livirov@gmail.com>
2024-10-24 23:29:25 +0300

    Improve rendering of the inline-member component in order to avoid blinking during navigation

--- pwa/src/app/components/inline-member/inline-member.component.ts
+++ pwa/src/app/components/inline-member/inline-member.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Injectable, Input, OnChanges, Optional, SimpleChanges } from '@angular/core';
 import { AsyncPipe, JsonPipe, NgClass, NgIf, NgOptimizedImage, NgStyle } from '@angular/common';
 import { AvatarService } from '../../services/avatar.service';
 import { User } from '../../models/user';
@@ -7,6 +7,38 @@ import { finalize } from 'rxjs';
 import { UserService } from '../../services/user.service';
 import { nameof } from '../../functions/name-of';
 import { getTextOrDefault } from '../../functions/get-text-or-default';
+
+interface CacheEntry<T> {
+  createdAt: number,
+  value: T
+}
+
+@Injectable({ providedIn: 'root' })
+class InlineMemberComponentCache {
+  private readonly CacheExpiration = 1000 * 60 * 60; /* One hour expiration */
+  private avatarUrlById: Map<number, CacheEntry<string>> = new Map<number, CacheEntry<string>>();
+
+  public getAvatarUrlById(id: number): string|undefined {
+    if (!this.avatarUrlById.has(id)) {
+      return undefined;
+    }
+
+    const cacheEntry = this.avatarUrlById.get(id);
+
+    if (cacheEntry.createdAt + this.CacheExpiration < Date.now()) {
+      return undefined;
+    }
+
+    return cacheEntry.value;
+  }
+
+  public setAvatarUrlForId(id: number, value: string) {
+    this.avatarUrlById.set(id, {
+      createdAt: Date.now(),
+      value: value
+    });
+  }
+}
 
 @Component({
   selector: 'app-inline-member',
@@ -38,7 +70,8 @@ export class InlineMemberComponent implements OnChanges {
 
   public constructor(
     private avatarService: AvatarService,
-    private userService: UserService
+    private userService: UserService,
+    private cache: InlineMemberComponentCache,
   ) {
   }
 
@@ -75,6 +108,11 @@ export class InlineMemberComponent implements OnChanges {
   }
 
   private loadAvatarUrl(): void {
+    this.avatarUrl = this.cache.getAvatarUrlById(this.profile.id);
+    if (this.avatarUrl) {
+      return;
+    }
+
     this.isLoading = true;
     this.avatarService
       .getUrl(this.profile)
@@ -83,6 +121,7 @@ export class InlineMemberComponent implements OnChanges {
       )
       .subscribe(url => {
         this.avatarUrl = url;
+        this.cache.setAvatarUrlForId(this.profile.id, url);
       });
   }
 }
